9737c8a55ddaade236f06ce144ff651701dfcc1d
david <david.pedrosa@staff.uni-marburg.de>
2022-07-18 00:56:38 +0200

    Added first steps into test.py

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ import mne
 from dependencies import ROOTDIR
 from utils.HelperFunctions import FileOperations
 import matplotlib.pyplot as plt
-
+from mne.preprocessing import (ICA, create_eog_epochs, create_ecg_epochs,
+                               corrmap)
 
 def countdown(n):
     """ very simple function intended to provide a countdown going form n to zero backwards """
@@ -24,9 +25,43 @@ def multiplication(k=10, n=5):
     return result
 
 
-def load_data():
+def load_and_preprocess_data():
     """ loads some data according to MNE tutorial """
     data_dir = os.path.join(ROOTDIR, 'data')
     files2test = FileOperations.list_files_in_folder(inputdir=data_dir, suffix='vhdr', entire_path=True)
-    dat_test = mne.io.read_raw_brainvision(files2test[0])
-    print(dat_test)
+    raw =  mne.io.read_raw_brainvision(files2test[0], preload=True, verbose=True)
+    raw.info['line_freq'] = 50
+    raw_downsampled = raw.copy().resample(sfreq=200)
+
+    for cutoff in (0.1, 4):
+        raw_highpass = raw_downsampled.copy().filter(l_freq=cutoff, h_freq=None)
+        with mne.viz.use_browser_backend('matplotlib'):
+            fig = raw_highpass.plot(duration=60, proj=False,
+                                    n_channels=len(raw_downsampled.ch_names), remove_dc=False)
+        fig.subplots_adjust(top=0.9)
+        fig.suptitle('High-pass filtered at {} Hz'.format(cutoff), size='xx-large',
+                     weight='bold')
+    # Set montage
+    montage = mne.channels.make_standard_montage('standard_1005')
+    # raw.set_montage(montage, verbose=True)
+
+    # Blink artifacts
+    raws = list()
+    icas = list()
+
+    for subj in range(4):
+        # EEGBCI subjects are 1-indexed; run 3 is a left/right hand movement task
+        fname = mne.datasets.eegbci.load_data(subj + 1, runs=[3])[0]
+        raw = mne.io.read_raw_edf(fname).load_data().resample(50)
+        # remove trailing `.` from channel names so we can set montage
+        mne.datasets.eegbci.standardize(raw)
+        raw.set_montage('standard_1005')
+        # high-pass filter
+        raw_filt = raw.copy().load_data().filter(l_freq=1., h_freq=None)
+        # fit ICA, using low max_iter for speed
+        ica = ICA(n_components=30, max_iter=100, random_state=97)
+        ica.fit(raw_filt, verbose='error')
+        raws.append(raw)
+        icas.append(ica)
+
+    raw.set_eeg_reference('average', projection=False, verbose=False)
