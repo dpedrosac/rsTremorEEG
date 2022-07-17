d05e9a02286c4141166a59a09b152b744c71d908
david <david.pedrosa@staff.uni-marburg.de>
2022-07-17 18:30:58 +0200

    Added functionality to test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 
+import os
+import numpy as np
+import mne
+from dependencies import ROOTDIR
+from utils.HelperFunctions import FileOperations
+import matplotlib.pyplot as plt
+
+
 def countdown(n):
     """ very simple function intended to provide a countdown going form n to zero backwards """
 
@@ -14,3 +22,11 @@ def multiplication(k=10, n=5):
 
     result = k*n
     return result
+
+
+def load_data():
+    """ loads some data according to MNE tutorial """
+    data_dir = os.path.join(ROOTDIR, 'data')
+    files2test = FileOperations.list_files_in_folder(inputdir=data_dir, suffix='vhdr', entire_path=True)
+    dat_test = mne.io.read_raw_brainvision(files2test[0])
+    print(dat_test)
--- utils/HelperFunctions.py
+++ utils/HelperFunctions.py
@@ -6,8 +6,6 @@ import os
 import re
 from itertools import groupby
 from operator import itemgetter
-
-
 from dependencies import ROOTDIR, GITHUB
 
 
