570b82224da181177bfc54e4d9396a4406333332
david <david.pedrosa@staff.uni-marburg.de>
2022-10-21 23:49:16 +0200

    Added some further analyses for single subject/single trial

--- test.py
+++ test.py
@@ -32,7 +32,33 @@ def load_and_preprocess_data():
     raw =  mne.io.read_raw_brainvision(files2test[0], preload=True, verbose=True)
     raw.info['line_freq'] = 50
     raw_downsampled = raw.copy().resample(sfreq=200)
-    raw_downsampled2 = raw.copy().resample(sfreq=100)
+
+    # Remove bad channels, that is those unrelated to EEG; TODO: what is the problem with o9 and 010 und why not keeping EKG?!
+    raw_downsampled.info['bads'].extend(['EKG', 'AccX', 'AccY', 'AccZ', 'EMG', 'SC', 'O9', 'O10'])
+    good_eeg = mne.pick_types(raw_downsampled.info, meg=False, eeg=True)
+    raw_downsampled.pick(good_eeg)
+    montage = mne.channels.make_standard_montage('standard_1005')
+    print(montage)
+
+    # Adding sensor information
+    raw_downsampled.set_montage(montage)
+
+    # Plot Power spectrum, if necessary
+    raw_downsampled.plot_psd(fmax=50)
+    raw_downsampled.plot(duration=5, n_channels=30)
+
+    # Artifact correcion; TODO a high-pass filter is recommended, which could be added here
+    ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
+    ica.fit(raw_downsampled)
+    ica.plot_components
+    ica.plot_sources(raw_downsampled, show_scrollbars=False)
+    ica.plot_overlay(raw, exclude=[17, 19], picks='eeg')
+
+    # TODO: Remove DBS artifact if present
+
+    # TODO: Find bad channels and interpolate them
+
+    # TODO: re-reference to common average
 
     for cutoff in (0.1, 4):
         raw_highpass = raw_downsampled.copy().filter(l_freq=cutoff, h_freq=None)
@@ -43,27 +69,7 @@ def load_and_preprocess_data():
         fig.suptitle('High-pass filtered at {} Hz'.format(cutoff), size='xx-large',
                      weight='bold')
     # Set montage
-    montage = mne.channels.make_standard_montage('standard_1005')
-    # raw.set_montage(montage, verbose=True)
 
     # Blink artifacts
     raws = list()
     icas = list()
-
-    for subj in range(4):
-        # EEGBCI subjects are 1-indexed; run 3 is a left/right hand movement task
-        fname = mne.datasets.eegbci.load_data(subj + 1, runs=[3])[0]
-        raw = mne.io.read_raw_edf(fname).load_data().resample(50)
-        # remove trailing `.` from channel names so we can set montage
-        mne.datasets.eegbci.standardize(raw)
-        raw.set_montage('standard_1005')
-        # high-pass filter
-        raw_filt = raw.copy().load_data().filter(l_freq=1., h_freq=None)
-        # fit ICA, using low max_iter for speed
-        ica = ICA(n_components=30, max_iter=100, random_state=97)
-        ica.fit(raw_filt, verbose='error')
-        raws.append(raw)
-        icas.append(ica)
-
-    raw.set_eeg_reference('average', projection=False, verbose=False)
-    #neuercomment
